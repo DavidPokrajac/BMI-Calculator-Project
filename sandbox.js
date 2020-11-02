ee16c535f6994ba54a0800be8f2f900efd3ec454
DavidPokrajac <d0pokrajac@gmail.com>
2020-11-02 10:39:53 +0100

    Initial commit

---
+++ sandbox.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+
+    // Europe container variables
+    var weight = document.querySelector("#weight");
+    var height = document.querySelector("#height");
+    var hi = document.querySelector("#hi");
+    var hey = document.querySelector("#hey");
+    var buttonOne = document.querySelector("#david");
+
+    // US container variables
+    var usHeightFt = document.querySelector("#us-height");
+    var usHeightIn = document.querySelector("#us-height-in");
+    var usWeight = document.querySelector("#us-weight");
+    var x = document.querySelector("#x");
+    var t = document.querySelector("#t");
+    var buttonTwo = document.querySelector("#kevin");
+
+
+}
